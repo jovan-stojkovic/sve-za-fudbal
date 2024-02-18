8f35f60b38b6a6e26324c5ab9edbf879de83cdac
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-02-19 00:18:32 +0100

    Navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./Styles.scss";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <Home />;
+  return (
+  <Router>
+      <Navbar />
+      <Home />
+      </Router>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,42 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return ( <>
-    this is the Navbar
-    </> );
-}
- 
+  return (
+    <>
+      <nav>
+        <div className="nav-container">
+          <div className="logo-container">
+            <a href="/">
+              <img
+                src="https://www.iconbolt.com/iconsets/hawcons-filled/soccer-court.svg"
+                alt="Footbal Field"
+              />
+            </a>
+            <h1 className="logo-headline">sve za fudbal.</h1>
+          </div>
+
+          <div className="navbar-links">
+            <Link>O Nama</Link>
+            <Link>Škole Fudbala</Link>
+            <Link>Baloni za Fudbal</Link>
+            <Link>Ostalo</Link>
+          </div>
+
+          <div className="navbar-buttons">
+            <button className="language-button">
+              <img src="./src/assets/flags/SRB.png" alt="SRB" />
+            </button>
+            <button className="theme-button">
+              <img
+                src="https://www.iconbolt.com/iconsets/weather-color/sun-sunny-temperature-weather.svg"
+                alt="Sun"
+              />
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
 export default Navbar;

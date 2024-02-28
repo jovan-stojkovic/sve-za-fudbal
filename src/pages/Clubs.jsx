0752e03fe770d22e23975b1cd8eecaab631052d3
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-02-28 17:24:26 +0100

    small changes

--- src/pages/Clubs.jsx
+++ src/pages/Clubs.jsx
@@ -1,4 +1,3 @@
-import { Dropdown } from "primereact/dropdown";
 import { useState, useEffect } from "react";
 
 const Clubs = () => {
@@ -11,10 +10,32 @@ const Clubs = () => {
 
     return () => clearTimeout(timer);
   }, []);
+
   return (
     <div className="clubs">
       <div className="photo-container">
-        <div className={`search-form ${isVisible ? "visible" : ""}`}></div>
+        <div className={`search-form ${isVisible ? "visible" : ""}`}>
+          <div className="search-club">
+            <label htmlFor="search">Pretraga po imenu</label>
+            <input type="text" placeholder="Pretraga..." id="search" />
+          </div>
+
+          <div className="city-part">
+            <button className="city-part-selector">Opstina</button>
+            <div className="city-parts">
+              <label htmlFor="cukarica">Čukarica</label>
+              <input type="radio" name="Čukarica" id="cukarica" />
+              <label htmlFor="rakovica">Rakovica</label>
+              <input type="radio" name="Rakovica" id="rakovica" />
+              <label htmlFor="novi-beograd">Novi Beograd</label>
+              <input type="radio" name="Novi Beograd" id="novi-beograd" />
+              <label htmlFor="palilula">Palilula</label>
+              <input type="radio" name="Palilula" id="palilula" />
+              <label htmlFor="vracar">Vračar</label>
+              <input type="radio" name="Vračar" id="vracar" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
